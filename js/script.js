cab477c02ff193e01bb841c074fe4577d1a13d35
GiorgioDellutri <giorgiodll93@gmail.com>
2022-12-05 11:39:10 +0100

    correction

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@
 //2.** Stampare a schermo attraverso un ciclo `for-in` tutte le proprietà dell'oggetto.
 //3. Creare un array di oggetti di studenti (ossìa oggetti con le proprietà sopra descritte) e mostriamoli.
 
-const student ={
+const student ={ 
     name : 'Sigismondo',
     surname : 'Mandragola',
     age : 77
 }
+console.log(student);
 
 for (let key in student){
-    console.log(student);
 }
