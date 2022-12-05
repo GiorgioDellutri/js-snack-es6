59219efcb7637e8c0855cd7656fba4062b368eb3
GiorgioDellutri <giorgiodll93@gmail.com>
2022-12-05 13:12:54 +0100

    snack 1

--- js/script.js
+++ js/script.js
@@ -2,14 +2,26 @@
 //2.** Stampare a schermo attraverso un ciclo `for-in` tutte le proprietà dell'oggetto.
 //3. Creare un array di oggetti di studenti (ossìa oggetti con le proprietà sopra descritte) e mostriamoli.
 
-const student ={ 
+const student =[
+    { 
     name : 'Sigismondo',
-    surname : 'Mandragola',
-    age : 77
-}
+    lastname : 'Mandragola',
+    age : 22
+    },
+    { 
+    name : 'Rob',
+    lastname : 'Rossi',
+    age : 33
+    },
+    { 
+    name : 'Jhon',
+    lastname : 'Deer',
+    age : 27
+    },
+];
 
-for (let key in student){
-    console.log(key, '=>', student[key])
+for (let i = 0; i < student.length; i++){
+    console.log(student[i])
 }
 
 
